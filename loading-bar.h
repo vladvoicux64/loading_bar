cdb7f060e92937268bb3ec014536f68081c66ee0
Voicu Ioan Vladut <77429938+vladvoicux64@users.noreply.github.com>
2022-09-11 15:21:55 +0300

    Update loading-bar.h

--- loading-bar.h
+++ loading-bar.h
@@ -11,7 +11,6 @@ using namespace std;
 
 void loading_bar(unsigned lenght, unsigned speed) { //lenght in characters, speed in ms/step, colour in ANSI format
     char uldd=177, ldd=219;
-    system( "COLOR 09");
     system("cls");
     printf("\e[?25l");
     cout<<"\n\n\n\n\t\tLoading...";
